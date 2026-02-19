b7587102e8033faa25cc4277a0f5d87d1a8bfb0e
Gabrielapassos2008 <gabrielpassos2008@email.com>
2026-02-18 23:13:19 -0300

    ...

--- src/main/java/com/example/primeiro_thymeleaf/Service/TaskService.java
+++ src/main/java/com/example/primeiro_thymeleaf/Service/TaskService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.primeiro_thymeleaf.model.Task;
 
--- src/main/java/com/example/primeiro_thymeleaf/controller/TaskController.java
+++ src/main/java/com/example/primeiro_thymeleaf/controller/TaskController.java
@@ -1,8 +1,5 @@
 package com.example.primeiro_thymeleaf.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
